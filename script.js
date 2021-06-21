a8baaae3afa2303ca79966395c40a89ab672277a
Azim <dryden1309@gmail.com>
2021-06-21 10:56:00 +0500

    masalalar

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+//Begin1
+console.log("Begin1");
+var a = 5;
+console.log(4 * a, "P = 20");
+console.log();
+
+//Begin2
+console.log("Begin2");
+console.log(a ** 2, "S=25");
+
+//Begin3
+console.log("Begin3");
+var b = 6;
+console.log(a * b, "S=30", 2 * (a + b), "P=22");
+
+//Begin4
+console.log("Begin4");
+var d = 8;
+var pi = 3.14;
+console.log(pi * d, "L=25.12");
+
+//Begin5
+console.log("Begin5");
+console.log(a ** 3, "V=125", 6 * a ** 2, "S=150");
+
+//Begin6
+console.log("Begin6");
+var c = 7;
+console.log(a * b * 7, "V=210", 2 * (a * b + b * c + a * c), "S=214");
+
+//Begin7
+console.log("Begin7");
+var R = 4;
+console.log(2 * pi * R, "L=25.12", pi * R ** 2, "S=50.24");
+
+//Begin8
+console.log("Begin8");
+console.log((a + b) / 2, "o'rta arifmetigi=5.5");
+//Begin9
+console.log("Begin9");
+console.log((a * b) ** 0.5);
+//console.log(Math.sqrt(a*b),"o'rta geometrigi =5.477225575051661")
+//Begin10
+console.log("Begin10");
+console.log("Yig'indisi", -6 + -5, 6 + 5);
+console.log("Ko'paytmasi", -6 * -5, 6 * 5);
+console.log("Kvadrati manfiy", (-6) ** 2, (-5) ** 2);
+console.log("Kvadrati musbat", 6 ** 2, 5 ** 2);
+//Begin11
+
+console.log("Begin11");
+console.log((a * b) ** 0.5);
+
+console.log("Yig'indisi", -6 + -5, 6 + 5);
+console.log("Ko'paytmasi", -6 * -5, 6 * 5);
+console.log("Modul 6, 5");
+console.log(Math.abs(-11, -57));
+
+//Begin12
+console.log("Begin12");
+var f = 10;
+console.log(Math.sqrt(b ** 2 + d ** 2), "Gipotenuza =10");
+console.log(b + d + f, "Perimetr=24");
+//Begin13
+console.log("Begin13");
+var r = 3;
+console.log(pi * R ** 2, "S1 = 50.24");
+console.log(pi * r ** 2, "S2 = 28.26");
+console.log(50.24 - 28.26, "S3 = 21.98");
+//Begin14
+console.log("Begin14");
+console.log(2 * pi * R, "Aylananing uzunligi L =25.12");
+console.log(pi * R ** 2, "S =50.24");
+//Begin15
+console.log("Begin15");
+console.log(2 * pi * R, "Aylananing yuzasi L =25.12");
+console.log(pi * R ** 2, "S =50.24");
+//Begin16
+console.log("Begin16");
+console.log(10 - 5, "Koordinata sistemasidagi 2 nuqta orasidagi masofa =5");
 //Begin17
 console.log("Begin17");
 console.log(Math.round(4.47), "AC = 4");
@@ -42,9 +122,9 @@ console.log(Math.round(25.45), "Uchburchak ning yuzi S = 25");
 //Beging22
 console.log("Begin22");
 
-var a = 5;
+// var a = 5;
 
-var b = 6;
+// var b = 6;
 console.log(a, b, "a=5 b=6");
 [a, b] = [b, a];
 
@@ -53,9 +133,9 @@ console.log(a, b, "a=6 b=5");
 
 //Begin23
 console.log("Begin23");
-var a = 5;
-var b = 6;
-var c = 7;
+// var a = 5;
+// var b = 6;
+// var c = 7;
 console.log(a, b, c, "a=5 b=6 c=7");
 [a, b, c] = [b, c, a];
 //console.log(`${a} ${b} ${c}`)
