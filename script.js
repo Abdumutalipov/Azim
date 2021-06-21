d71dcc79cbc8ffb5dc54b20d82e0b74426e5556c
Azim <dryden1309@gmail.com>
2021-06-21 10:52:36 +0500

    dsdsdsd

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-document.body.innerHTML = '<h1>Hellow from JavaScript</h1>'
+//Begin17
+console.log("Begin17");
+console.log(Math.round(4.47), "AC = 4");
+console.log(Math.round(3.16), "BC = 3");
+console.log(4 + 3);
+//Begin18
+console.log("Begin18");
+console.log(Math.round(2.82), "AC = 3");
+console.log(Math.round(2.82), "BC = 3");
+console.log(3 * 3, "AC va BC ning ko`paytmasi =9");
+//Begin19
+console.log("Begin19");
+console.log(2 * (6 + 4), "Perimetri  P=20");
+console.log(4 * 6, "S =24");
+//Begin20
+console.log("Begin20");
+
+var x1 = 12;
+var x2 = 9;
+var y1 = 2;
+var y2 = 11;
+console.log(
+  Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2),
+  "Ikki nuqta orasi = 9.48"
+);
+//Begin21
+console.log("Begin21");
+var x1 = 12;
+var x2 = 9;
+var x3 = 6;
+var y1 = 2;
+var y2 = 11;
+var y3 = 2;
+console.log(Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2));
+console.log(Math.round(9.48), "AB = 9");
+console.log(Math.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2));
+console.log(Math.round(9.48), "AC = 9");
+console.log(Math.sqrt((x3 - x1) ** 2 + (y3 - y1) ** 2), "CB = 6");
+console.log((9 + 9 + 6) * 0.5, "Perimetrning yarimi = 12");
+console.log(Math.sqrt(12 * (12 - 9) * (12 - 9) * (12 - 6)));
+console.log(Math.round(25.45), "Uchburchak ning yuzi S = 25");
+//Beging22
+console.log("Begin22");
+
+var a = 5;
+
+var b = 6;
+console.log(a, b, "a=5 b=6");
+[a, b] = [b, a];
+
+//console.log((`${a} ${b}`));
+console.log(a, b, "a=6 b=5");
+
+//Begin23
+console.log("Begin23");
+var a = 5;
+var b = 6;
+var c = 7;
+console.log(a, b, c, "a=5 b=6 c=7");
+[a, b, c] = [b, c, a];
+//console.log(`${a} ${b} ${c}`)
+console.log(a, b, c, "a=6 b=7 c=5");
